4175d451ec93f71b70a64faa788d9685fa74ba03
NatarazNarendra <narendraz88912@gmail.com>
2020-11-17 18:28:45 +0530

    functional Interface16112020

---
+++ src/main/java/com/mkyong/jdbc/FunctionalInterfaceExample2Test.java
@@ -0,0 +1,17 @@
+package com.mkyong.jdbc;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FunctionalInterfaceExample2Test {
+
+	FunctionalInterfaceExample2 fc=new FunctionalInterfaceExample2();
+	
+	public void show(int i) {
+		System.out.println(10);
+		
+	}
+	
+
+}
