25256c8ee29c82067609b148eb38cbac2d6a4a74
NatarazNarendra <narendraz88912@gmail.com>
2020-11-17 23:24:06 +0530

    Java does not support multiple inheritances but we can achieve the effect of multiple inheritances using interfaces. In interfaces, a class can implement more than one interface which can’t be done through extends keyword. Please refer Multiple inheritance in java for more.

--- src/main/java/TestInterface.java
+++ src/main/java/TestInterface.java
@@ -1,7 +1,7 @@
 @FunctionalInterface
 public interface TestInterface {
 public abstract void show(int n);
-public abstract void show1(int n);
+//public abstract void show1(int n);
 public static void hello() {
 System.out.println("static method");
 }
