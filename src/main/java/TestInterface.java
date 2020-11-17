0970d3aaeac27008a5030eb9e0f29506a887e10a
NatarazNarendra <narendraz88912@gmail.com>
2020-11-17 18:55:15 +0530

    Invalid '@FunctionalInterface' annotation; TestInterface is not a functional  interface

---
+++ src/main/java/TestInterface.java
@@ -0,0 +1,11 @@
+@FunctionalInterface
+public interface TestInterface {
+public abstract void show(int n);
+public abstract void show1(int n);
+public static void hello() {
+System.out.println("static method");
+}
+public default void test() {
+System.out.println("default method");
+}
+}
